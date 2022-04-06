0f0d5e06f746a570343c4452e3bd190669b6e4f7
DuncanHacker <bence.horvath8@gmail.com>
2022-04-06 19:33:46 +0200

    Album WPF

--- C0AGQP_HFT_2021221.WPFClient/AlbumWindow.xaml.cs
+++ C0AGQP_HFT_2021221.WPFClient/AlbumWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace C0AGQP_HFT_2021221.WPFClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AlbumWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AlbumWindow : Window
     {
-        public MainWindow()
+        public AlbumWindow()
         {
             InitializeComponent();
-        }
-
-        private void Albums_Click(object sender, RoutedEventArgs e)
-        {
-            AlbumWindow albumWindow = new AlbumWindow();
-            albumWindow.Show();
         }
     }
 }
